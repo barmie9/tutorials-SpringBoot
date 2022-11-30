abe6c9b695cd6aefbd5489c812dfe63d46510945
barmie9 <bartek.mierzwinski.99@gmail.com>
2022-11-30 17:45:14 +0100

    Own methods Spring Data, Paging

--- src/main/java/com/example1/SpringBootTutorial/repository/PostRepository.java
+++ src/main/java/com/example1/SpringBootTutorial/repository/PostRepository.java
@@ -1,9 +1,48 @@
 package com.example1.SpringBootTutorial.repository;
 
 import com.example1.SpringBootTutorial.model.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
+
+    //First method:
+//    @Query("select p from Post p where title = ?1")
+//    List<Post> findAllByTitle(String title);
+
+    //Second method:
+//    @Query("select p from Post p where title = :title")
+//    List<Post> findAllByTitle(@Param("title") String title);
+
+    //3th method. In Java >=8  We can add Param to compiler
+    // We must add some code to build.gradle
+//    @Query("select p from Post p where title = :title")
+//    List<Post> findAllByTitle(String title);
+
+    //4th method for simple select:
+//    List<Post> findAllByTitle(String title);
+    // Why it works? Look at this:
+    //https://docs.spring.io/spring-data/jpa/docs/2.2.7.RELEASE/reference/html/#jpa.query-methods
+
+
+
+    //Resolve problem with query N + 1 (Many queries in database)
+
+    //Żeby popbrać powiązaną Encje należu użyć słowa "fetch"
+//    @Query("select p from Post p " +
+//            "left join fetch p.comment")
+//    List<Post> findAllPosts();
+
+    // Paging (Stronicowanie):
+    @Query("select p from Post p " +
+            "left join fetch p.comment")
+    List<Post> findAllPosts(Pageable page);
+
+
 }
--- src/main/java/com/example1/SpringBootTutorial/service/PostService.java
+++ src/main/java/com/example1/SpringBootTutorial/service/PostService.java
@@ -3,6 +3,7 @@ package com.example1.SpringBootTutorial.service;
 import com.example1.SpringBootTutorial.model.Post;
 import com.example1.SpringBootTutorial.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public class PostService {
     private final PostRepository postRepository;
 
     public List<Post> getPosts(){
-        return postRepository.findAll();
+        return postRepository.findAllPosts(PageRequest.of(0,5)); //Param use to paging (Stronicowanie)
     }
     public Post getSinglePost(long id){
         return postRepository.findById(id).orElseThrow();
